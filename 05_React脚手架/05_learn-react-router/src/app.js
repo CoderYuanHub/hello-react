40e11daffe7eda50add44fb14867011a5b40259f
wyy <807515004@qq.com>
2021-10-08 14:55:10 +0800

    feat: 补充Router使用内容

--- 05_React脚手架/05_learn-react-router/src/app.js
+++ 05_React脚手架/05_learn-react-router/src/app.js
@@ -5,7 +5,9 @@ import {
     NavLink,
     Link,
     Route,
-    Switch
+    Switch,
+    Redirect,
+    withRouter
 } from 'react-router-dom'
 
 // class Home extends PureComponent {
@@ -34,17 +36,58 @@ import {
 function Index() {
     return <div>这是首页</div>
 }
-function About() {
-    return <div>这是管乐</div>
+function About(props) {
+    return (
+        <div>
+            {/* 嵌套路由的使用 */}
+            <NavLink exact activeClassName="about-link" to="/about">文化</NavLink>
+            <NavLink activeClassName="about-link" to="/about/food">餐饮</NavLink>
+            <NavLink activeClassName="about-link" to="/about/join">加入</NavLink>
+            <button onClick={e => changeRouter(props)}>手动跳转</button>
+            <Route exact path="/about" component={AountCulture} />
+            <Route path="/about/food" component={AountFood} />
+            <Route path="/about/join" component={AountJion} />
+        </div>
+    )
+    function changeRouter(props) {
+        // 嵌套在路由组件会在props传递history、location、match三个属性
+        props.history.push('/login')
+        console.log(props);
+    }
 }
-function Me() {
-    return <div>这是我</div>
+function AountCulture() {
+    return <div>这是文化</div>
 }
+function AountFood() {
+    return <div>这是餐饮</div>
+}
+function AountJion() {
+    return <div>这是加入</div>
+}
+function Login() {
+    return <div>登陆组件</div>
+}
+class Me extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            isLogin: false
+        }
+    }
+    render() {
+        return (
+            <div>
+                {this.state.isLogin ? (<div>个人信息</div>) : <Redirect to="/login" component={Login} />}
+            </div>
+        )
+    }
+}
+
 function NotMatch() {
     return <div>没有匹配到</div>
 }
 
-export default class App extends PureComponent {
+class App extends PureComponent {
     render() {
         return (
             <div>
@@ -56,19 +99,29 @@ export default class App extends PureComponent {
                     <Route path="/about" component={About} />
                     <Route exact path="/" component={Index} />
                 </BrowserRouter> */}
-                <HashRouter>
-                    <NavLink exact to="/">首页</NavLink>
-                    <NavLink to="/about">关于</NavLink>
-                    <NavLink to="/me">我的</NavLink>
-                    <NavLink to="/other">其他</NavLink>
-                    <Switch>
-                        <Route exact path="/" component={Index} />
-                        <Route path="/about" component={About} />
-                        <Route path="/me" component={Me} />
-                        <Route component={NotMatch} />
-                    </Switch>
-                </HashRouter>
+                {/* NavLink 自带选中效果 通过activeStyle可以修改选中样式 */}
+                {/* NavLink 自带选中效果 activeClassName可以修改选中时候的class名称,默认自带active属性 */}
+
+                <NavLink exact to="/" activeStyle={{ fontSize: "20px", color: "red" }}>首页</NavLink>
+                <NavLink to="/about" activeStyle={{ fontSize: "20px", color: "red" }}>关于</NavLink>
+                <NavLink to="/me" activeStyle={{ fontSize: "20px", color: "red" }}>我的</NavLink>
+                <NavLink to="/other" activeStyle={{ fontSize: "20px", color: "red" }}>其他</NavLink>
+                <button onClick={e => this.changeRouter()}>手动跳转</button>
+
+                {/* Switch用于排他，当找到匹配项的时候就显示第一个找到项 */}
+                <Switch>
+                    <Route exact path="/" component={Index} />
+                    <Route path="/about" component={About} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/me" component={Me} />
+                    <Route component={NotMatch} />
+                </Switch>
+
             </div>
         )
     }
+    changeRouter() {
+        this.props.history.push('/login')
+    }
 }
+export default withRouter(App);
